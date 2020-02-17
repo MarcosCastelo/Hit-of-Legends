966d0c67b64909226fda453f637203a6a3c4fee0
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-02-17 18:10:36 -0300

    Inimigos morrem com hit

--- src/Objects/Bullet.gd
+++ src/Objects/Bullet.gd
@@ -6,15 +6,14 @@ var velocity: = Vector2.ZERO
 
 func _ready() -> void:
 	velocity = (get_global_mouse_position() - global_position).normalized() * speed
-	print(velocity)
 
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity = velocity.bounce(collision.normal)
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
